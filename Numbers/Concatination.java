8c753d9ef224d3b35ad95a2ebde8bb9f3e7488f7
Rakshitha0019 <244g1a05w5@srit.ac.in>
2026-02-12 01:08:25 +0530

    Add files via upload

---
+++ Numbers/Concatination.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+class Concatination {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int con=(a*10)+b;
+        // Write Your Code to Concatenate and print
+       System.out.println(con);
+    }
+}
