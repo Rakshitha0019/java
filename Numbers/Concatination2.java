ab98a79829c2b465b5763e8852a42df708c81638
Rakshitha0019 <244g1a05w5@srit.ac.in>
2026-02-16 00:46:36 +0530

    Add files via upload

---
+++ Numbers/Concatination2.java
@@ -0,0 +1,10 @@
+class  Concatination2
+{
+    public static void main(String args[])
+    {
+        int n=1234;
+        int m=77;
+        int result=(m*100)+(n%100);
+        System.out.println(result);
+    }
+}
