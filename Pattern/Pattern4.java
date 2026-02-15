aee87ac51ab758f0e09e794858530987f7b94a83
Rakshitha0019 <244g1a05w5@srit.ac.in>
2026-02-15 21:25:05 +0530

    Add files via upload

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Pattern4
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+       System.out.println("enter n value");
+       int n=sc.nextInt();
+       int i,j;
+       for(i=0;i<n;i++)
+       {
+        for(j=0;j<n;j++)
+        {
+            if(i*j==0 || (i*j%4)==0)
+             System.out.print("* ");
+            else 
+             System.out.print("  ");
+        }
+       System.out.print("\n");
+
+             
+       } 
+    }
+
+    
+}
