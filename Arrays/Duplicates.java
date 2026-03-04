c320cd9222ff5b89d1278703446f0188b646e59b
Rakshitha0019 <244g1a05w5@srit.ac.in>
2026-03-04 21:16:48 +0530

    Add files via upload

---
+++ Arrays/Duplicates.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Duplicates {
+    public static void main(String Args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter n value");
+       int n=sc.nextInt();
+       int found=0;
+       int[] nums=new int[n];
+       System.out.println("enter"+ n + " value into array");
+       for(int i=0;i<n;i++)
+       {
+        nums [i] =sc.nextInt();
+       }
+        Arrays.sort(nums);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+               found=1;
+               break;
+            }
+            
+            
+        }
+        if(found==1)
+        {
+          System.out.println("Duplicates found"); 
+        }
+        else
+         System.out.println(" no Duplicates found"); 
+        
+      
+    }
+}
