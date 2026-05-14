688862405598347ad98d21d030d1468f764e0a72
Rakshitha0019 <244g1a05w5@srit.ac.in>
2026-05-14 22:23:11 +0530

    Add files via upload

---
+++ ExceptionHandling/MultipleCatch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.*;
+public class MultipleCatch
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        System.out.println("enter b value");
+        int b=sc.nextInt();
+        int a[]={23,19,3,8,20};
+        try{
+            int res=n/b;
+            System.out.println("enter value to access");
+            int i=sc.nextInt();
+            System.out.println(a[i]);           
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Cannot be divided"+e.getMessage());
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("enter valid index"+e.getMessage());
+        }
+        catch(Exception e)
+        {
+          System.out.println("Error!!!!!"+e.getMessage());  
+        }
+
+    }
+}
