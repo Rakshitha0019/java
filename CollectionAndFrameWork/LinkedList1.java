584f0a2f3581538e426b352e89d153a1719006f0
Rakshitha0019 <244g1a05w5@srit.ac.in>
2026-06-03 22:29:21 +0530

    Add files via upload

---
+++ CollectionAndFrameWork/LinkedList1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class LinkedList1{
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+
+        l.add(10);
+        l.add(20);
+        l.add(30);
+
+        System.out.println(l);
+    }
+}
